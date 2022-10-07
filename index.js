065b9e528fd1f385f039a25527be1e2170d96a61
Arunkumarpothitla <luckyarunkumar00@gmail.com>
2022-10-07 15:43:50 +0530

    commit msg

---
+++ index.js
@@ -0,0 +1,7 @@
+const names = [
+    { firstName: "Arunkumar", lastName: "Pothitla" },
+    { firstName: "Ashok", lastName: "Nijamgari" },
+    { firstName: "Hari", lastName: "Golla" },
+];
+var surname = names.map((value) => value.lastName);
+console.log(surname);
